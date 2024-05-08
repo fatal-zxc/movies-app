24f9eb41bc979cf76ca5da38de141974a93f0426
fatal-zxc <faaatal_l@proton.me>
2024-05-08 19:25:51 +0300

    mobile

--- src/components/MovieCard/movie-card.js
+++ src/components/MovieCard/movie-card.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Card, Flex, Typography, Space, Rate } from 'antd'
 import { format } from 'date-fns'
 
@@ -18,42 +18,69 @@ function overviewShorter(text) {
 
 const { Title, Text } = Typography
 
-function MovieCard(props) {
-  const { title, overview, poster, release, rate } = props
-  const rateRounded = (Math.round(rate * 10) / 10).toFixed(1)
-  const overviewShort = overviewShorter(overview)
-  return (
-    <Card hoverable className="card" styles={{ body: { padding: 0, overflow: 'hidden' } }}>
-      <Flex>
-        <img
-          alt="poster"
-          src={`https://image.tmdb.org/t/p/w200${poster}`}
-          className="poster"
-        />
-        <Flex vertical align="flex-start" style={{ marginLeft: 20 }}>
-          <Title level={3} style={{ marginTop: 10, marginBottom: 7, fontSize: 20, width: 180 }}>
-            {title}
-          </Title>
-          <Text type="secondary" style={{ marginBottom: 7, fontSize: 12 }}>
-            {`${format(release, 'MMMM d')}, ${format(release, 'y')}`}
-          </Text>
-          <Space style={{ marginBottom: 7 }}>
-            <Text className="genre">Action</Text>
-            <Text className="genre">Drama</Text>
-          </Space>
-          <Text className="overview">{overviewShort}</Text>
-          <Rate
-            allowHalf
-            disabled
-            defaultValue={rateRounded}
-            count={10}
-            className="stars"
-          />
-          <p className="rate">{rateRounded}</p>
-        </Flex>
-      </Flex>
-    </Card>
-  )
-}
+export default class MovieCard extends Component {
+  constructor() {
+    super()
 
-export default MovieCard
+    const { rate, overview } = this.state
+
+    this.state = {
+      rateRounded: (Math.round(rate * 10) / 10).toFixed(1),
+      overviewShort: overviewShorter(overview),
+    }
+  }
+
+  render() {
+    const mobile = document.body.clientWidth <= 500
+    const { title, poster, release } = this.props
+    const { rateRounded, overviewShort } = this.state
+
+    return (
+      <Card
+        hoverable
+        className="card"
+        styles={{ body: { padding: 0, overflow: 'hidden' } }}
+      >
+        <Flex>
+          <img
+            alt="poster"
+            src={`https://image.tmdb.org/t/p/w200${poster}`}
+            className="poster"
+          />
+          <Flex
+            vertical
+            align="flex-start"
+            style={{ marginLeft: 20 }}
+          >
+            <Title
+              level={3}
+              style={{ marginTop: 10, marginBottom: 7, fontSize: 20, width: 180 }}
+            >
+              {title}
+            </Title>
+            <Text
+              type="secondary"
+              style={{ marginBottom: 7, fontSize: 12 }}
+            >
+              {`${format(release, 'MMMM d')}, ${format(release, 'y')}`}
+            </Text>
+            <Space style={{ marginBottom: 7 }}>
+              <Text className="genre">Action</Text>
+              <Text className="genre">Drama</Text>
+            </Space>
+            {!mobile ? <Text className="overview">{overviewShort}</Text> : null}
+            <Rate
+              allowHalf
+              disabled
+              defaultValue={rateRounded}
+              count={10}
+              className="stars"
+            />
+            <p className="rate">{rateRounded}</p>
+          </Flex>
+        </Flex>
+        {mobile ? <Text className="overview">{overviewShort}</Text> : null}
+      </Card>
+    )
+  }
+}
--- src/components/MoviesList/movies-list.js
+++ src/components/MoviesList/movies-list.js
@@ -1,6 +1,7 @@
 import { Component } from 'react'
 import { Flex } from 'antd'
 
+import './movies-list.css'
 import MovieCard from '../MovieCard'
 import MoviesApi from '../../services/movies-api'
 
@@ -15,13 +16,12 @@ export default class MoviesList extends Component {
     MoviesApi.a = new MoviesApi()
 
     this.cardsUpdate = () => {
-      MoviesApi.a.getMoviesList(1)
-        .then((res) => {
-          const newData = res.results.slice(0, 6)
-          this.setState({
-            data: newData,
-          })
+      MoviesApi.a.getMoviesList(1).then((res) => {
+        const newData = res.results.slice(0, 6)
+        this.setState({
+          data: newData,
         })
+      })
     }
     this.cardsUpdate()
   }
@@ -42,12 +42,17 @@ export default class MoviesList extends Component {
             release={data[i].release_date}
             rate={data[i].vote_average}
             key={data[i].id}
-          />,
+          />
         )
       }
     }
     return (
-      <Flex wrap="wrap" justify="center" gap={36}>
+      <Flex
+        wrap="wrap"
+        className="moviesList"
+        justify="center"
+        gap={36}
+      >
         {moviesCards}
       </Flex>
     )
